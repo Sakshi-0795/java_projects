3d536fbecc3d94b1623a9b11b4beb55195b39457
Sakshi-0795 <moresakshi796@gmail.com>
2025-04-26 21:30:36 +0530

    My Object Program
    I have just print number in this program

---
+++ key.java
@@ -0,0 +1,16 @@
+public class key{
+    int a=2,b=3,c=8,d=12,e=76;
+    public static void main(String[]args){
+        key myobj1=new key();
+        key myobj2=new key();
+        key myobj3=new key();
+        key myobj4=new key();
+        key myobj5=new key();
+        System.out.println(myobj1.a);
+        System.out.println(myobj2.b);
+        System.out.println(myobj3.c);
+        System.out.println(myobj4.d);
+        System.out.println(myobj5.e);
+
+    }
+}
