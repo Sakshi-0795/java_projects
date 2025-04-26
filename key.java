f08f853284cffd871ab38d3bdaa125f9674d8716
Sakshi-0795 <moresakshi796@gmail.com>
2025-04-26 21:46:52 +0530

    Update key.java
    I have added my name obejct.

--- key.java
+++ key.java
@@ -1,16 +1,22 @@
 public class key{
-    int a=2,b=3,c=8,d=12,e=76;
+    int a=2,b=312,c=8,d=12,e=76;
+    String name = "My Name is Sakshi";
+    int age = "My age is 20";
+    
     public static void main(String[]args){
         key myobj1=new key();
         key myobj2=new key();
         key myobj3=new key();
         key myobj4=new key();
         key myobj5=new key();
+        key myobj6=new key();
         System.out.println(myobj1.a);
         System.out.println(myobj2.b);
         System.out.println(myobj3.c);
         System.out.println(myobj4.d);
         System.out.println(myobj5.e);
+        System.out.println(myobj6.name);
+
 
     }
 }
