42ea6ba1384d5af5f701221b2b977be93fc6214d
Sakshi-0795 <moresakshi796@gmail.com>
2025-05-11 21:49:48 +0530

    sample project

---
+++ bag.java
@@ -0,0 +1,9 @@
+class bag{
+    public static void main(String[]args){
+        String str="sakshi";
+        String str2="more";
+
+        str2.concat("more");
+        System.out.println(str2);
+    }
+}
