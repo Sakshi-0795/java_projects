b44d1fd0737e6393403ae5a928e3dc376f30c32f
Sakshi more <moresakshi796@gmail.com>
2026-05-09 23:23:25 +0530

    Add files via upload

---
+++ SC.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class SC{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String name = sc.next();
+
+        System.out.println("Hello welcom " + name);
+    }
+}
+
